e359396b6baef6dd6c72a6a688324590f5f8034c
tiago_gomes <tiago@omure.com>
2021-09-21 09:19:41 +0100

    Added exceptions config Added salesforce config

---
+++ config/exceptions.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    "oauth_failed_in_salesforce_rest_api" => "Oauth failed in salesforce rest api",
+];
--- config/salesforce.php
+++ config/salesforce.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    ""
+    "oauth_url" => env('SALESFORCE_AUTHORIZE'),
+    "callback_url" => env('SALESFORCE_CALLBACK_URL'),
+    "salesforce_key" => env('SALESFORCE_KEY'),
+    "salesforce_secret" => env('SALESFORCE_SECRET')
 ];
