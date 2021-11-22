b29d3fdbf2285fb7ac99661f92adb5dfe9b2af4c
tiago-gomes <tiago@omure.com>
2021-11-22 15:30:40 +0000

    ignore this dummy commit

--- app/Http/Controllers/SalesForceController.php
+++ app/Http/Controllers/SalesForceController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Routing\Redirector;
 use Illuminate\Http\JsonResponse;
 
+
 class SalesForceController extends Controller
 {
     private SalesForceApi $salesForceApi;
